5b78b871bd5b77184d30d0cbfda6b355fc6d1209
Martynas Girdziuna <martynas.girdziuna@gmail.com>
2019-12-12 22:35:08 +0200

    Front page done
    the buttons dont take you anywhere, however its simple to fix, just create a file in folder and change the name of destination in view file

--- includes/main.php
+++ includes/main.php
@@ -7,14 +7,83 @@ class Model {
 
     private $conn;
 
+        function __construct()
+    {
+        $this->setDefaultSessions();
+        date_default_timezone_set("Europe/Vilnius");
+        $dbConfigFile = fopen("./includes/database.config", "r") or die("Unable to open file!");
+        $dbConfigFileString =  fgets($dbConfigFile);
+        $dbConfigLines = explode(":", $dbConfigFileString);
+        fclose($dbConfigFile);
+        $this->server = $dbConfigLines[0];
+        $this->dbUser = $dbConfigLines[1];
+        $this->dbPassword = $dbConfigLines[2];
+        $this->dbName = $dbConfigLines[3];
+        $this->conn = new mysqli($this->server, $this->dbUser, $this->dbPassword, $this->dbName);
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+        $this->updateLoginStatus();
+    }
+
     public function logoutMe()
     {
         $_SESSION['id'] = "0";
-        $_SESSION['slapyvardis'] = "0";
+        $_SESSION['vardas'] = "0";
         $_SESSION['slaptazodis'] = "0";
         $_SESSION['email'] = "0";
         $_SESSION['role'] = "0";
         $_SESSION['uzblokuotas'] = "0";
+
+        return true;
+    }
+
+        public function updateLoginStatus()
+    {
+        if($_SESSION['id'] != "0")
+        {
+            $username = $this->secureInput($_SESSION['vardas']);
+            $password = $this->secureInput($_SESSION['slaptazodis']);
+
+            $sql = "SELECT * FROM naudotojas WHERE vardas='$username'";
+            $result = $this->conn->query($sql);
+
+            if ($result->num_rows > 0)
+            {
+                while($row = $result->fetch_assoc())
+                {
+                    if($password == $row['slaptazodis'])
+                    {
+                        $_SESSION['id'] = $row['id'];
+                        $_SESSION['vardas'] = $row['vardas'];
+                        $_SESSION['slaptazodis'] = $row['slaptazodis'];
+                        $_SESSION['email'] = $row['email'];
+                        $_SESSION['role'] = $row['role'];
+                        $_SESSION['uzblokuotas'] = $row['uzblokuotas'];
+                        return true;
+                    }
+                    else
+                    {
+                        $this->logoutMe();
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                $this->logoutMe();
+            }
+        }
+    }
+
+        public function setDefaultSessions()
+    {
+        // If ID is not set, it means we have to set default sessions, instead of rewriting whole function, I call logout() function. It does the same thing.
+        if(!isset($_SESSION['id']) && empty($_SESSION['id']))
+        {
+            $this->logoutMe();
+        }
 
         return true;
     }
---
+++ includes/view.php
@@ -0,0 +1,69 @@
+<?php
+class View
+{
+
+    function __construct()
+    {
+
+    }
+
+
+public static function printNavbar($location)
+    {
+        echo '<a class="navbar-brand" href="index.php">Gaming Forumas  </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">';
+        self::printNavbarItem("Namai", "index.php", $location);
+        if($_SESSION['uzblokuotas'] !== '1') {
+            self::printNavbarItem("Forumas", "forum.php", $location);
+            self::printNavbarItem("Galerija", "gallery.php", $location);
+        }
+        if ($_SESSION['role'] == "0") {
+            self::printNavbarItem("Registruotis", "register.php", $location);
+            self::printNavbarItem("Prisijungti", "login.php", $location);
+        } else {
+            if ($_SESSION['role'] >= 2 && $_SESSION['uzblokuotas'] !== '1') {
+                self::printNavbarItem("Valdymas", "adminpanel.php", $location);
+            }
+            self::printNavbarItem("Nustatymai", "settings.php", $location);
+            self::printNavbarItem("Atsijungti", "logout.php", $location);
+        }
+        if( $_SESSION['uzblokuotas'] !== '1') {
+            echo '</ul>
+            <form class="form-inline my-2 my-lg-0" method="POST" action="search.php">
+                <input class="form-control mr-sm-2" type="search" name="searchText" placeholder="Raktažodis paieškai" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Ieškoti</button>
+            </form>
+            </div>
+          </nav>
+        ';
+        }
+
+    }
+
+        private static function printNavbarItem($name, $location, $globalLocation)
+    {
+        if ($globalLocation == $location) {
+            echo '
+                  <li class="nav-item active">
+                    <a class="nav-link" href="' . $location . '">' . $name . '</a>
+                  </li>';
+        } else {
+            echo '
+                  <li class="nav-item">
+                    <a class="nav-link" href="' . $location . '">' . $name . '</a>
+                  </li>';
+        }
+    }
+
+        function printIndexPage()
+    {
+        echo '<h1>Sveiki atvykę į filmu nuoma!</h1>';
+    }
+}
+
+?>
