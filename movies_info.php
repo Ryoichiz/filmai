574cefd931c32c1e4b23afb2578453c7040e5333
Martynas Girdziuna <martynas.girdziuna@gmail.com>
2019-12-15 19:57:43 +0200

    More navigation

--- movies_info.php
+++ movies_info.php
@@ -82,7 +82,7 @@ if(isset($_GET['ID'])){
 		<?php
     	MainController::printTopBar("movies.php");
     	?>
-    	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    	<nav class="navbar navbar-expand-lg navbar-inverse bg-dark">
         <div class='container'>
             <?php
                 MainController::printNavigationBar("movies.php");
